adc4d09c63f52c84482b88f6e9ac85172fcd3043
camper0008 <spetakel123@gmail.com>
2021-12-20 16:18:29 +0100

    file clearing and response status codes

--- src/api.rs
+++ src/api.rs
@@ -1,10 +1,11 @@
-use rocket::response::content;
-
-use crate::file_storage::{file_as_bytes, file_name_array};
+use rocket::response::content::Json;
+use rocket::response::status::{NotFound, Custom};
+use rocket::http::Status;
+use crate::file_storage::{file_as_bytes, file_name_vector, clear_files_stored};
 
 #[get("/filelist")]
-pub fn route_file_list<'a>() -> content::Json<String> {
-    match file_name_array() {
+pub fn route_file_list<'a>() -> Result<Json<String>, Custom<Json<String>>> {
+    match file_name_vector() {
         Ok(v) => {
             let fold = v
                 .iter()
@@ -17,18 +18,18 @@ pub fn route_file_list<'a>() -> content::Json<String> {
                         }
                     }
                 );
-            content::Json(format!("{{\"files\":[{}]}}", fold).to_string())
+            Json(format!("{{\"files\":[{}]}}", fold).to_string())
         } 
-        Err(_) => content::Json("{\"files\":[]}".to_string())
+        Err(_) => Err(Custom(Status::InternalServerError, Json("{\"msg\":\"an error occurred trying to get file list\"}".to_string())))
     }
 }
 
 #[get("/files/<file_name>")]
-pub fn route_download_file(file_name: String) -> Vec<u8> {
+pub fn route_download_file(file_name: String) -> Result<Vec<u8>, NotFound<Json<String>>> {
     let res = file_as_bytes(file_name);
     match res {
-        Ok(v) => v,
-        Err(_) => "file not found".as_bytes().to_vec(),
+        Ok(v) => Ok(v),
+        Err(_) => Err(NotFound(Json("{\"msg\": \"file not found\"}".to_string()))),
     } 
 }
 
@@ -38,6 +39,9 @@ pub fn route_upload_files() -> &'static str {
 }
 
 #[post("/clear")]
-pub fn route_clear_files() -> &'static str {
-    "Hello, world!"
+pub fn route_clear_files() -> Result<Json<String>, Custom<Json<String>>> {
+    match clear_files_stored() {
+        Ok(_) => Ok(Json("{\"msg\":\"files cleared successfully\"}".to_string())),
+        Err(_) => Err(Custom(Status::InternalServerError, Json("{\"msg\":\"an error occurred trying to clear files\"}".to_string()))),
+    }
 }
